d90a5ee1c4a18db16ae8ce78717ea26c7915faae
Cecilia Manner <ceciliamannerr@gmail.com>
2025-03-06 11:29:36 +0100

    firebase connection

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBK7bV1tQ89Fv9zBgzVTrXpYw7X_e8_wt4",
+    authDomain: "tasks-workshop-50703.firebaseapp.com",
+    projectId: "tasks-workshop-50703",
+    storageBucket: "tasks-workshop-50703.firebasestorage.app",
+    messagingSenderId: "420550030677",
+    appId: "1:420550030677:web:4f313a128f21b573536bdc"
+  };
+
+  const app = initializeApp(firebaseConfig); 
+
+  export default app; 
